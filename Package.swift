cb6dfb0a11f811fa80586c0103f73e60527f66de
Ehlen, David <david.ehlen@dm.de>
2020-02-27 22:02:36 +0100

    add test target

--- Package.swift
+++ Package.swift
@@ -19,8 +19,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Core",
-            dependencies: []),
+        .target(name: "Core", dependencies: []),
+        .testTarget(name: "CoreTests", dependencies: ["Core"])
     ]
 )
